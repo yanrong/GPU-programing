b31d4312d9c24e591ff3bf2e797fc133ab5e2328
yanrong <yanrongdu@gmai.com>
2021-11-19 16:55:30 +0800

    add learn opengl common files

---
+++ opengl-tutorial/learn-opengl/common/fileSystem.hpp
@@ -0,0 +1,21 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+
+#include <string>
+#include <cstdlib>
+#include "root_directory.h"
+
+class fileSystem{
+private:
+    typedef std::string (*Builder) (const std::string &path);
+
+public:
+    static std::string getPath(const std::string& path);
+
+private:
+    static std::string const & getRoot();
+    static Builder getPathBuilder();
+    static std::string getPathRelativeRoot(const std::string &path);
+    static std::string getPathRelativeBinary(const std::string &path);
+};
+#endif
