335580795c053b78591b6d7eab107d59f3acac15
yanrong <yanrongdu@gmai.com>
2021-12-24 16:40:12 +0800

    add learn opengl assimp code

---
+++ opengl-tutorial/learn-opengl/common/mesh.hpp
@@ -0,0 +1,46 @@
+#ifndef _MESH_H_
+#define _MESH_H_
+
+#include <string>
+#include <vector>
+
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "common/shader_s.hpp"
+
+#define MAX_BONE_INFLUENCE 4
+struct vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 textCoord;
+    glm::vec3 tangent;
+    glm::vec3 bitAngent;
+
+    int mBoneIds[MAX_BONE_INFLUENCE];
+    float mWeights[MAX_BONE_INFLUENCE];
+};
+
+struct texture {
+    unsigned int id;
+    std::string type;
+    std::string path;
+};
+
+class mesh {
+public:
+    std::vector<vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<texture> textures;
+    GLuint VAO;
+
+    mesh(std::vector<vertex> vertices, vector<unsigned int> indices, vector<texture> textures);
+    void draw(Shader &shader);
+private:
+    GLuint VBO, EBO;
+    void setupMesh();
+}
+
+#endif /* end of _MESH_H_ */
